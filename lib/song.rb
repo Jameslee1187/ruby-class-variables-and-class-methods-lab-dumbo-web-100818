82a89f33f2104d3cee1f55654ee2a1d4575526d9
jameslee1187 <jameslee1187@gmail.com>
2018-10-14 17:21:22 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+class Song
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    @@songs = []
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@songs.push(name)
+    @@genres.push(genre)
+    @@artists.push(artist)
+  end
+    
+    def self.count
+      @@count
+    end
+    
+    def self.songs
+      @@songs
+    end
+    
+    def self.artists
+      @@artists.uniq
+    end
+    
+    def self.genres
+      @@genres.uniq
+    end
+    
+
+end
